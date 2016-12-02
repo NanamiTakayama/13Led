7d677649d5ad2fa884bb9ed629f34798108dbaef
NanamiTakayama <ns.potable@gmail.com>
2016-12-02 19:16:03 +0900

    update from hikarie

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var v = 0;
 setInterval(function(){
 	v = v ? 0:1;
 	if(v){
-		console.log("LED ON with isaax");
+		console.log("LED ON with isaax hikarie");
 		led.dir(mraa.DIR_OUT);
 	}else{
-		console.log("LED OFF with isaax");
-	}led.write(v);},5000);
+		console.log("LED OFF with isaax hikarie");
+	}led.write(v);},500);
 

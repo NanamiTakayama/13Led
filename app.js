98b04e63be4139aa1eff4420e42f677e33192ff9
NanamiTakayama <ns.potable@gmail.com>
2016-12-28 17:50:22 +0900

    122502

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var mraa = require("mraa");
+//var mraa = require("mraa");
 var led = new mraa.Gpio(13); 
 var v = 0;
 console.log("____AppStarted____");
@@ -7,8 +7,8 @@ setInterval(function(){
 	v = v ? 0:1;
 	if(v){
 		console.log("LED ON with isaax 1209");
-		led.dir(mraa.DIR_OUT);
+		//led.dir(mraa.DIR_OUT);
 	}else{
 		console.log("LED OFF with isaax 1209");
-	}led.write(v);},500000);
+	}led.write(v);},90000);
 
